913b8c830c2dda05fc65531f0f46e4a6bf5e0014
hanniestars <hanniemarquez@gmail.com>
2014-05-25 15:24:28 +0800

    Revert "friendship"
    This reverts commit ea8f5eca4f23092d754c0423538f63fdc9e9457a.

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
 	resources :users
 	resources :friendships
 	resource :posts
-	root 'users#show' 
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
